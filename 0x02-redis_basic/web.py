b2402a1f5f218e6217fbe14523a4cc11932b0a11
okoroafor wisdom onyinye <104413525+wisdomoo@users.noreply.github.com>
2023-07-20 05:51:58 +0100

    Create web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+web cache and tracker
+"""
+
+import redis
+import requests
+from functools import wraps
+
+r = redis.Redis()
+
+
+def url_access_count(method):
+    """decorator for get_page function"""
+    @wraps(method)
+    def wrapper(url):
+        """wrapper function"""
+        key = "cached:" + url
+        cached_value = r.get(key)
+        if cached_value:
+            return cached_value.decode("utf-8")
+
+            # Get new content and update cache
+        key_count = "count:" + url
+        html_content = method(url)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
+        return html_content
+    return wrapper
+
+
+@url_access_count
+def get_page(url: str) -> str:
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk'
